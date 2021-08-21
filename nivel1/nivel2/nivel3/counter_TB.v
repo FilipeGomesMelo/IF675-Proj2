226d3a43147b17225932ec78ddfbf548923db81d
FilipeGomesMelo <61971614+FilipeGomesMelo@users.noreply.github.com>
2021-08-21 13:59:36 -0300

    Começo dos contadores

---
+++ nivel1/nivel2/nivel3/counter_TB.v
@@ -0,0 +1,62 @@
+`timescale 1ns / 1ps
+
+module counter_tb ();
+
+    // Inputs
+    reg [3:0] data;
+    reg loadn, clearn, clock, enable;
+
+    // Outputs
+    wire [3:0] digit;
+    wire tc, zero;
+
+    counter_mod10 uut (data, loadn, clearn, clock, enable, digit, tc, zero);
+
+    initial begin
+
+        $dumpfile("counter_waves.vcd");
+        $dumpvars(0,counter_tb);
+
+        // TESTE BASE
+        assign loadn = 1;
+        assign enable = 0; 
+        assign clearn = 1;
+        assign data = 4'b0110;
+
+        #5 assign loadn = 0; 
+
+        #6 assign loadn = 1; 
+        #5 assign enable = 1; 
+
+        // CASO ENABLE E LOADN ESTEJAM ATIVOS AO MESMO TEMPO (deve dar erro)
+        // assign loadn = 1; 
+        // assign enable = 1; 
+        // assign clearn = 1;
+        // assign data = 4'b0110;
+
+        // #5 assign loadn = 0;
+
+        // #5 assign loadn = 1; 
+
+        // TESTE DO CLEARN
+        // assign loadn = 1; 
+        // assign enable = 0; 
+        // assign clearn = 1;
+        // assign data = 4'b0110;
+
+        // #5 assign loadn = 0;
+
+        // #5 assign loadn = 1; 
+        // #5 assign enable = 1; 
+
+        // #50 assign clearn = 0;
+        // assign enable = 0;
+
+        #400;
+        $finish;
+    end
+
+    initial clock = 0;
+    always #10 clock = !clock;
+
+endmodule
---
+++ nivel1/nivel2/nivel3/counter_mod6.v
@@ -0,0 +1,45 @@
+module counter_mod10 (
+    input wire [3:0] data,
+    input wire loadn, // sicrono, active low
+    input wire clearn, // assincrono, active high
+    input wire clock,
+    input wire en, // active high
+    output reg [3:0] digit,
+    output wire tc, // vira high quando o counter chega a 0 para ativar o prox contador da cadeia
+    output wire zero); // indica se o contador chegou a 0
+
+    assign tc = ((digit == 0) & en) ? 1 : 0;
+    assign zero = (digit == 0) ? 1 : 0;
+
+    // limpa o digit de forma assincrona quando clearn está ativo (acive low)
+    always @ (negedge clearn) begin
+        digit <= 4'b0000;
+    end
+
+    // digit recebe data de forma sincrona quando loadn está ativo (active low)
+    // loop principal para a contagem
+    always @(posedge clock) begin
+        // so conta quando en está ativo (active high)
+        if (!loadn) digit <= data;
+        else if (en) begin
+            case (digit)
+                4'b0110: digit <= 4'b0101; // 6 -> 5
+                4'b0101: digit <= 4'b0100; // 5 -> 4
+                4'b0100: digit <= 4'b0011; // 4 -> 3
+                4'b0011: digit <= 4'b0010; // 3 -> 2
+                4'b0010: digit <= 4'b0001; // 2 -> 1
+                4'b0001: begin // 1 -> 0
+                    digit <= 4'b0000;
+                    // tc <= 1'b1;
+                    // zero <= 1'b1;
+                end
+                default: begin // 0 -> 9
+                    digit <= 4'b0110;
+                    // tc <= 1'b0;
+                    // zero <= 1'b0;
+                end
+            endcase
+        end  
+    end
+endmodule
+
